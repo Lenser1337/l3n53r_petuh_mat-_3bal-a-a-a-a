7562917b80e1a8239c410d0e4915e43710f5e189
unknown <prostobogdob@gmail.com>
2018-11-15 16:18:25 +0200

    fix poisk

--- commands/find.js
+++ commands/find.js
@@ -5,12 +5,13 @@ module.exports.run = async (bot, message, args) => {
   let mymessage = args.join(" ").slice(22);
   let pnchannel = message.guild.channels.find(`name`, "👋поиск_напарников");
 
-    const embed = new Discord.RichEmbed()
-    .setTitle("ИЩУ НАПАРНИКА")
-    .addField("Сообщение:", `${mymessage}`, true)
-    .setColor("#4C8BF5")
-
-    pnchannel.send({embed});
+    // const embed = new Discord.RichEmbed()
+    // .setTitle("ИЩУ НАПАРНИКА")
+    // .addField("Сообщение:", `${mymessage}`, true)
+    // .setColor("#4C8BF5")
+    //
+    // pnchannel.send({embed});
+    pnchannel.send({mymessage});
 }
 
 module.exports.help = {
