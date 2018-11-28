da4acb88fa9e865fe07528f1bba07e000672df2c
Alexis Murer <alexis@ottspott.co>
2018-11-28 13:47:39 +0100

    test

--- commands/scanuser.js
+++ commands/scanuser.js
@@ -182,12 +182,15 @@ module.exports.run = async (bot, message) => {
 					//проверка на мут и выдача роли (на случай если чел ливнул)
 
 					if(foundObj.mutedUntil){
+						message.channel.send("DB1");
 						var muterole = message.guild.roles.find(`name`, "Наручники (Мут чата)");
 						var now = new Date();
 						var ts = Math.floor(now/1000);
 						var tsLimit = Math.floor(foundObj.mutedUntil/1000);
-						if (ts < tsLimit)
+						if (ts < tsLimit){
+							message.channel.send("DB2");
 							await(message.member.addRole(muterole.id));
+						}
 					}
 
 					if (timestampLimit < timestamp) {
