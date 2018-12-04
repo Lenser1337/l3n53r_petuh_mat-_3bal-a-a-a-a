79f78ac32b2ccbb349df56467834eb8c17307e45
DJS3M3N <spiryaev.semen@mail.ru>
2018-12-05 01:30:30 +0400

    Fix

--- commands/ganginfo.js
+++ commands/ganginfo.js
@@ -69,7 +69,7 @@ module.exports.run = async (bot, message, args) => {
                 value:  `${foundObj.leader}`
               },
               {
-                name: `***Участников***`
+                name: `***Участников***`,
                 value: `${foundObj.membersAmount}`
               }
             ],
