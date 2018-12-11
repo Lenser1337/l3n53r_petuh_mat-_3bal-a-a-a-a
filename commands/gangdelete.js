58b8ee813c8c15999825743bd81f3ec70918d4e4
alexismurer <alexis.murer@gmail.com>
2018-12-11 02:04:51 +0100

    my bad

--- commands/gangdelete.js
+++ commands/gangdelete.js
@@ -42,7 +42,7 @@ module.exports.run = async (bot, message, args) => {
         return message.reply("ты не являешься главарём какой-либо группировки!");
       }
       else {
-        if (user_obj.membersAmount > 1)
+        if (foundObj.membersAmount > 1)
           return message.reply("нельзя удалить группироку, в которой есть участники!");
         var leha = message.guild.members.find("id", "215970433088880641");
         var sema = message.guild.members.find("id", "354261484395560961");
