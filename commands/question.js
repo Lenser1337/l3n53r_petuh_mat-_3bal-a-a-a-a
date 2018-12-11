84e1e328c3c512ce664ac35c1613d3228a0cf3ac
alexismurer <alexis.murer@gmail.com>
2018-12-11 15:34:00 +0100

    question

---
+++ commands/question.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URL);
+var User = require('./../schemas/user_model.js');
+var Question = require('./../schemas/question_model.js');
+
+function random(min, max) {
+	var result = Math.floor(Math.random() * (max - min + 1)) + min;
+	return (result);
+}
+
+module.exports.run = async (bot, message, args) => {
+  message.reply("хуй тебе :D");
+}
+
+module.exports.help = {
+  name: "question"
+}
