3242e333897d3dacc0cef617f3586bc70e690e68
alexismurer <alexis.murer@gmail.com>
2018-12-13 21:11:20 +0100

    fix

--- commands/gangpay.js
+++ commands/gangpay.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
 var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;mongoose.connect(process.env.MONGO_URL);
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URL);
 var User = require('./../schemas/user_model.js');
 var Gang = require('./../schemas/gang_model.js');
 
@@ -12,10 +13,10 @@ const numberWithCommas = (x) => {
 
 function formatDate(date) {
   var monthNames = [
-  "января", "февраля", "марта",
-  "апреля", "мая", "июня", "июля",
-  "августа", "сентября", "октября",
-  "ноября", "декабря"
+    "января", "февраля", "марта",
+    "апреля", "мая", "июня", "июля",
+    "августа", "сентября", "октября",
+    "ноября", "декабря"
   ];
 
   var day = date.getDate();
@@ -30,74 +31,14 @@ function formatDate(date) {
 }
 
 module.exports.run = async (bot, message, args) => {
- var user_obj = await User.findOne({userID: message.member.id}, function (err, foundObj){});
- if (user_obj.gang == null || typeof user_obj.gang == 'undefined')
-   return message.reply("ты не находишься в группировке!");
- var gangName = user_obj.gang;
- var cash = user_obj.retrocoinCoin;
- var retricIcon = bot.emojis.find("name", "retric");
+  var user_obj = await User.findOne({userID: message.member.id}, function (err, foundObj){});
+  if (user_obj.gang == null || typeof user_obj.gang == 'undefined')
+    return message.reply("ты не находишься в группировке!");
+  var gangName = user_obj.gang;
+  var cash = user_obj.retrocoinCoin;
+  var retricIcon = bot.emojis.find("name", "retric");
 
- 	if (args[0] === "all"){
-    var user_obj = await User.findOne({
-			userID: message.member.id
-		}, function (err, foundObj) {
-			if (err)
-				console.log("Error on database findOne: " + err);
-			else {
-				if (!foundObj)
-					console.log("Something stange happend");
-				else {
-					if (foundObj.retrocoinCash === 0)
-						return message.reply("чеееее :thinking: У тебя в кармане пусто!");
-					else {
-						var actBank = foundObj.retrocoinBank;
-						var actCash = foundObj.retrocoinCash;
-						var newCash = 0;
-
-						foundObj.retrocoinCash = newCash;
-						foundObj.retrocoinTotal = newCash + actBank;
-						foundObj.save(function(err, updatedObj){
-							if(err)
-								console.log(err);
-						})
-
-						var avatar = message.member.user.avatarURL;
-						var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
-
-						const embed = new Discord.RichEmbed()
-						.setTitle(`Все ретрики отправлены на счет группировки! Новый баланс ${message.member.displayName}`)
-						.setColor("#0000FF")
-						.addField("Наличкой", `${numberWithCommas(foundObj.retrocoinCash)} ${retricIcon}`, true)
-						.addField("В банке", `${numberWithCommas(foundObj.retrocoinBank)} ${retricIcon}`, true)
-
-						message.channel.send({embed});
-					}
-				}
-			}
-		});
-
-    var user_obj = await Gang.findOne({
-      name: gangName
-    }, function(err, foundObj){
-      if (err)
-				console.log("Error on database findOne: " + err);
-			else {
-				if (!foundObj)
-					console.log("Something stange happend");
-				else {
-          var balance = foundObj.balance;
-          var newbalance = balance + cash;
-          foundObj.balance = newbalance;
-          foundObj.save(function(err, updatedObj){
-            if(err)
-              console.log(err);
-          })
-        }
-      }
-    })
-  }
-
-  else if (isNumeric(args[0])){
+  if (isNumeric(args[0])){
     var toWith = Number(args[0]);
     var user_obj = await User.findOne({
       userID: message.member.id
@@ -111,50 +52,51 @@ module.exports.run = async (bot, message, args) => {
           var actBank = foundObj.retrocoinBank;
           var actCash = foundObj.retrocoinCash;
           var newCash = actCash - toWith;
-            if(newCash < 0)
-              message.reply(`у тебя нехватает ретриков на данное действие!`)
-            foundObj.retrocoinCash = newCash;
-            foundObj.retrocoinTotal = actBank + newCash;
-            foundObj.save(function(err, updatedObj){
-              if(err)
-                console.log(err);
-            })
-            var avatar = message.member.user.avatarURL;
-            var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
-            const embed = new Discord.RichEmbed()
-            .setTitle(toWith + " ретриков отправлено на счет группировки! Новый баланс " + message.member.displayName)
-            .setColor("#0000FF")
-            .addField("Наличкой", `${numberWithCommas(foundObj.retrocoinCash)} ${retricIcon}`, true)
-            .addField("В банке", `${numberWithCommas(foundObj.retrocoinBank)} ${retricIcon}`, true)
+          if(newCash < 0)
+            message.reply(`у тебя нехватает ретриков на данное действие!`)
+          foundObj.retrocoinCash = newCash;
+          foundObj.retrocoinTotal = actBank + newCash;
+          foundObj.save(function(err, updatedObj){
+            if(err)
+              console.log(err);
+          })
+          var avatar = message.member.user.avatarURL;
+          var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
+          const embed = new Discord.RichEmbed()
+          .setTitle(toWith + " ретриков отправлено на счет группировки! Новый баланс " + message.member.displayName)
+          .setColor("#0000FF")
+          .addField("Наличкой", `${numberWithCommas(foundObj.retrocoinCash)} ${retricIcon}`, true)
+          .addField("В банке", `${numberWithCommas(foundObj.retrocoinBank)} ${retricIcon}`, true)
 
-            message.channel.send({embed});
-          }
+          message.channel.send({embed});
+
+          var user_obj = Gang.findOne({
+            name: gangName
+          }, function (err, foundObj) {
+            if (err)
+              console.log("Error on database findOne: " + err);
+            else {
+              if (!foundObj)
+                console.log("Something stange happend");
+              else {
+                var balance = foundObj.balance;
+                var newbalance = balance + toWith;
+                foundObj.balance = newbalance;
+                foundObj.save(function(err, updatedObj){
+                  if(err)
+                    console.log(err);
+                })
+              }
+            }
+          });
         }
-      })
-      var user_obj = await Gang.findOne({
-        name: gangName
-      }, function (err, foundObj) {
-        if (err)
-          console.log("Error on database findOne: " + err);
-        else {
-          if (!foundObj)
-            console.log("Something stange happend");
-          else {
-            var balance = foundObj.balance;
-            var newbalance = balance + toWith;
-            foundObj.balance = newbalance;
-            foundObj.save(function(err, updatedObj){
-							if(err)
-								console.log(err);
-						})
-          }
-        }
-      })
-    }
-          else {
-            return message.channel.send(`У тебя разве хватает ${retricIcon} (ретриков) на такое действие? :thinking:`);
-          }
+      }
+    })
   }
+  else {
+    return message.channel.send(`У тебя разве хватает ${retricIcon} (ретриков) на такое действие? :thinking:`);
+  }
+}
 
 module.exports.help = {
   name: "gangpay"
