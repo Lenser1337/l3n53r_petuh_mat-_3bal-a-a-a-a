9ffd423eba9bc6481c0e2afd4587e3acc3280a8b
alexismurer <alexis.murer@gmail.com>
2018-12-15 02:51:06 +0100

    test5

--- commands/test.js
+++ commands/test.js
@@ -4,17 +4,15 @@ var Jimp = require('jimp');
 module.exports.run = async (bot, message, args) => {
   if(!message.member.roles.some(r=>["Тех. Администратор", "Губернатор", "Тех. Стажер"].includes(r.name)))
     return;
+  console.log('Process.env.PWD is: ' + process.env.PWD);
 
-  Jimp.read('http://www.travelandstyle.ca/wp-content/uploads/2015/07/Hotel-Valley-Ho-Vintage-Pool.jpg')
-  .then(image => {
-    message.channel.send("1");
-    image.resize(256, 256)
-//    .write("https://retrobotproject.herokuapp.com/images/test-small.jpg");
-    .write("test-small.jpg");
-  })
-  .catch(err => {
-    console.log(err);
-  });
+  // var images = [];
+  // var jimps = [];
+  //
+  // for (var i = 0; i < images.length; i++){
+  //   jimps.push(Jimp.read(images[i]));
+  // }
+
 
 }
 
