5eaf7e16300d62ed2f4e763239b0ba2ca874b8c7
alexismurer <alexis.murer@gmail.com>
2018-12-16 20:21:26 +0100

    find2

--- commands/find.js
+++ commands/find.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
 var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;mongoose.connect(process.env.MONGO_URL);
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URL);
 var User = require('./../schemas/user_model.js');
 
 function random(min, max) {
--- commands/find2.js
+++ commands/find2.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.roles.some(r=>["Тех. Администратор", "Губернатор",].includes(r.name)))
-    return;
   message.delete().catch(O_o=>{});
 
   if (message.member.voiceChannel) {
