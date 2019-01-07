5be3919181ec2f72e15c499da0a353bb78798d66
DJS3M3N <spiryaev.semen@mail.ru>
2019-01-07 23:34:35 +0400

    Fix

--- commands/sayDM.js
+++ commands/sayDM.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+  var dmchannel = bot.channels.find(`id`, "531815935544131594");
   let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   let sayMessage = "";
   sayMessage = args.join(" ").slice(22);
@@ -10,6 +11,21 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.roles.some(r=>["Тех. Администратор", "Губернатор", "Стример", "Тех. Стажер"].includes(r.name)))
     return;
 
+    dmchannel.send({embed: {
+      color: 3447003,
+      title: `Отправлено...`,
+      fields: [
+      {
+        name: `Сообщение`,
+        value: sayMessage
+      }
+      ],
+      timestamp: new Date(),
+      footer: {
+        icon_url: user.avatarURL,
+        text: `${user.username}`
+      }
+    }
   message.delete().catch();
   user.send(sayMessage);
 }
