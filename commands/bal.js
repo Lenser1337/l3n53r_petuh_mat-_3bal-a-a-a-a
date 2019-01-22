4e22c52c400f008acc76969c50a2cf2d0a59cbf8
Alexis Murer <alexis@ottspott.co>
2019-01-22 13:59:43 +0100

    bal

--- commands/bal.js
+++ commands/bal.js
@@ -32,13 +32,20 @@ module.exports.run = async (bot, message, args) => {
 					console.log("User not found in database");
 					return;
 				}
+
+				var bank = foundObj.retrocoinBank;
+
+				if (bank === Infinity){
+					bank = "реально дофига ретриков";
+				}
+
 				var avatar = message.member.user.avatarURL;
 				var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
 				const embed = new Discord.RichEmbed()
 				.setTitle("Личный счет " + message.member.displayName)
 				.setColor("#0000FF")
 				.addField("Наличкой", `${numberWithCommas(foundObj.retrocoinCash)} ${retricIcon}`, true)
-				.addField("В банке", `${numberWithCommas(foundObj.retrocoinBank)} ${retricIcon}`, true)
+				.addField("В банке", `${numberWithCommas(bank)} ${retricIcon}`, true)
 				.setThumbnail(avatar)
 
 				message.channel.send({embed});
@@ -53,13 +60,20 @@ module.exports.run = async (bot, message, args) => {
 			else {
 				if (foundObj === null)
 					return;
+
+				var bank = foundObj.retrocoinBank;
+
+				if (bank === Infinity){
+					bank = "реально дофига ретриков";
+				}
+
 				var avatar = toScan.user.avatarURL;
 				var total = foundObj.retrocoinCash + foundObj.retrocoinBank;
 				const embed = new Discord.RichEmbed()
 				.setTitle("Личный счет " + toScan.displayName)
 				.setColor("#0000FF")
 				.addField("Наличкой", `${numberWithCommas(foundObj.retrocoinCash)} ${retricIcon}`, true)
-				.addField("В банке", `${numberWithCommas(foundObj.retrocoinBank)} ${retricIcon}`, true)
+				.addField("В банке", `${numberWithCommas(bank)} ${retricIcon}`, true)
 				.setThumbnail(avatar)
 
 				message.channel.send({embed});
