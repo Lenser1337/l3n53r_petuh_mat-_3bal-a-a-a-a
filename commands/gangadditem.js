c7776d936cd07cc229aec41222bcd4d8ba8a0ab9
alexismurer <alexis.murer@gmail.com>
2019-01-26 20:40:13 +0100

    fix 2

--- commands/gangadditem.js
+++ commands/gangadditem.js
@@ -7,16 +7,15 @@ var Item = require('./../schemas/gangshop_model.js');
 module.exports.run = async (bot, message, args) => {
 
   message.delete(3000);
-  //лимит который нужно прописать во все комманды что бы никто другой пока что не использовал
-  // if(!message.member.hasPermission("MANAGE_ROLES"))
-  //   return;
-  if(!message.member.roles.some(r=>["Тех. Администратор", "Губернатор"].includes(r.name)))
+
+  if(!message.member.roles.some(r=>["Тех. Администратор"].includes(r.name)))
     return;
 
-  message.delete().catch(O_o=>{});
+  //message.delete().catch(O_o=>{});
 
   if(!args)
     return message.reply(`название / цена / уровень`).then(msg => msg.delete(10000));
+
   var itm = "";
   var prc = 1;
   var level = 1;
@@ -26,6 +25,7 @@ module.exports.run = async (bot, message, args) => {
     var newStr = message.cleanContent.split('"').pop();
     prc = newStr.split(" ", 2).pop();
     console.log("price: " + prc);
+    console.log("newStr: " + newStr);
   }
   else {
     itm = args[0];
