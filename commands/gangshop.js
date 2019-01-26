6d1374b857168d7e31636a0ebbde916e14150692
alexismurer <alexis.murer@gmail.com>
2019-01-26 20:19:06 +0100

    fix

--- commands/gangshop.js
+++ commands/gangshop.js
@@ -4,6 +4,7 @@ const ms = require("ms");
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;mongoose.connect(process.env.MONGO_URL);
 var Item = require('./../schemas/gangshop_model.js');
+var User = require('./../schemas/user_model.js');
 
 const numberWithCommas = (x) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
