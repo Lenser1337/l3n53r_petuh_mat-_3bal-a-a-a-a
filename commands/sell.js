aedc988692dd32a46287fb591ab2e07730a92db8
DJS3M3N <spiryaev.semen@mail.ru>
2019-01-27 01:14:23 +0400

    Fix

--- commands/sell.js
+++ commands/sell.js
@@ -44,8 +44,8 @@ module.exports.run = async (bot, message, args) => {
 	if (user_obj.inv.includes(item_obj.itemName) == false)
 		return message.reply(`у тебя нету ${item_obj.itemName}`);
 
-		var index = user_obj.inv.indexOf(item.itemName);
 		var newinv = user_obj.inv;
+		newinv.splice(index, 1);
     user_obj.inv = newinv;
 		var price = item_obj.itemPrice / 2;
     user_obj.retrocoinBank = user_obj.retrocoinBank + price;
