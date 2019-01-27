8435471c532f1473aa44ea04533d80f9584950ff
alexismurer <alexis.murer@gmail.com>
2019-01-27 05:43:00 +0100

    test

--- commands/scangang.js
+++ commands/scangang.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;mongoose.connect(process.env.MONGO_URL);
 var User = require('./../schemas/user_model.js');
 var Gang = require('./../schemas/gang_model.js');
 
-function deletegang(gang) {
+function deleteGang(gang) {
   var gang_obj = Gang.deleteOne({
     name: gang.name
   }, function(err, obj) {
@@ -15,50 +15,77 @@ function deletegang(gang) {
   });
 }
 
-function removerole(userID, role, leaderID) {
-var user = message.guild.members.find("id", userID);
-var leader = message.guild.members.find("id", leaderID);
-user.removeRole(role);
-leader.removeRole(role);
-var user_obj = User.findOne({
-  userID: userID
-}, function (err, foundObj) {
-  if (err)
-    console.log("Error on database findOne: " + err);
-  else {
-    foundObj.gang = null;
-    foundObj.save(function(err, updatedObj){
-      if(err)
-        console.log(err);
-    });
-  }
-});
-leader.sendMessage("У тебя удалена группировка из-за недостатка участников");
+function removeGangFromUser(user_id, role) {
+  var user = message.guild.members.find("id", user_id);
+
+  user.removeRole(role);
+  var user_obj = User.findOne({
+    userID: user_id
+  }, function (err, foundObj) {
+    if (err)
+      console.log("Error on database findOne: " + err);
+    else {
+      foundObj.gang = null;
+      foundObj.save(function(err, updatedObj){
+        if(err)
+          console.log(err);
+      });
+    }
+  });
 }
 
-function checkGangLevel(gang) {
+function removeGangFromLeader(user_id, role) {
+  var user = message.guild.members.find("id", user_id);
+
+  user.removeRole(role);
+  var user_obj = User.findOne({
+    userID: user_id
+  }, function (err, foundObj) {
+    if (err)
+      console.log("Error on database findOne: " + err);
+    else {
+      foundObj.gang = null;
+      foundObj.leaderOf = null;
+      foundObj.save(function(err, updatedObj){
+        if(err)
+          console.log(err);
+      });
+    }
+  });
+}
+
+function checkGang(gang) {
+
   var dateTime = Date.now();
   var timestampLimit = Math.floor(gang.created/1000) + 3600;
   var gangRole = message.guild.roles.find(`name`, gang.name);
   if (gang.level = 1 && dateTime >= timestampLimit){
-   deletegang(gang);
-   gang.otherMembers.forEach(function(user_id) {
-  removerole(user_id, gangRole, gang.leaderID);
-   });
- }
+    deleteGang(gang);
+    gang.otherMembers.forEach(function(user_id) {
+      removeGangFromUser(user_id, gangRole);
+    });
+    var leader = message.guild.members.find("id", gang.leaderID);
+    leader.sendMessage("У тебя удалена группировка из-за недостатка участников! Как и предупреждалось, нужно было набрать 5 человек в течении часа... Увы!");
+    removeGangFromLeader(user_id, gangRole);
+    console.log("Successfully deleted gang: " + gang.name);
+  }
 }
 
 module.exports.run = async (bot, message, args) => {
 
   var gangs = Gang.find({level:1}).lean().exec(function(err, gangstab) {
-    if (gangstab == null || typeof gangstab == "undefined")
-      return console.log("gangs is undefined");
-    else
-      console.log("length is: " + gangstab.length);
+    if(err)
+      console.log(err);
+    else{
+      if (gangstab == null || typeof gangstab == "undefined")
+        return console.log("gangs is undefined");
+      else
+        console.log("length is: " + gangstab.length);
 
-    gangstab.forEach(function(gang) {
-      //checkGangLevel(gang);
-    });
+      gangstab.forEach(function(gang) {
+        checkGang(gang);
+      });
+    }
   });
   // var leha = message.guild.members.find("id", "215970433088880641");
   // var sema = message.guild.members.find("id", "354261484395560961");
