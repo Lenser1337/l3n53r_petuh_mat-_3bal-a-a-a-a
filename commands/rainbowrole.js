d12a9271559212f7159681f465fb0f436ff51787
DJS3M3N <spiryaev.semen@mail.ru>
2019-02-04 00:30:39 +0400

    Undo

--- commands/rainbowrole.js
+++ commands/rainbowrole.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot) => {
   if (!guild)
     return console.log("No guild found");
 
-  var rainbowrole = guild.roles.find("name", "Радуга");
+  var rainbowrole = guild.roles.find("id", "541714178595880992");
 
   if(!rainbowrole)
     return console.log("No role found");
