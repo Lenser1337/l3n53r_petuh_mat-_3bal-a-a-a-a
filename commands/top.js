ace5dc6836f368c2d133ec771837cfb504c3e583
DJS3M3N <spiryaev.semen@mail.ru>
2019-02-05 15:33:05 +0400

    Fix

--- commands/top.js
+++ commands/top.js
@@ -4,18 +4,13 @@ const ms = require("ms");
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;mongoose.connect(process.env.MONGO_URL);
 var User = require('./../schemas/user_model.js');
-
 const numberWithCommas = (x) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
-
 module.exports.run = async (bot, message, args) => {
-
   var retricIcon = bot.emojis.find("name", "retric");
-
   // if(!message.member.roles.some(r=>["Тех. Администратор", "Губернатор", "🚨РетроТестер🚨"].includes(r.name)))
   //   return;
-
   var topusers = User.find().sort({retrocoinTotal: -1}).limit(60).lean().exec(function(err, doc) {
     if(err)
       console.log(err);
@@ -64,13 +59,10 @@ module.exports.run = async (bot, message, args) => {
       else {
         return message.reply("можно просмотреть только 50 самых богатых пользователей сервера, укажи страницу с 1 по 5!")
       }
-
       var y = 0;
       var text = ``;
-
       while(x < maxX)
         text += `**${y=x+1}.** ${doc[x].displayName} • **${numberWithCommas(doc[x++].retrocoinTotal)} ретрика(ов)**\n`;
-
       message.channel.send({embed: {
         color: message.member.highestRole.color,
         title: `**Retro Valley** :zap: **LEADERBOARD**`,
@@ -87,11 +79,9 @@ module.exports.run = async (bot, message, args) => {
         },
       }
     });
-
   }
 });
 }
-
 module.exports.help = {
   name: "top"
 }
