f2f812a8dadaac0100d846475130d20f3e254866
DJS3M3N <spiryaev.semen@mail.ru>
2019-02-11 05:23:29 +0400

    -_-

--- commands/4r.js
+++ commands/4r.js
@@ -26,7 +26,6 @@ function formatDate(date) {
   var second = date.getSeconds();
   var time = hour + ":" + minute + ":" + second;
 
-
   return day + ' ' + monthNames[monthIndex] + ' ' + year + ', ' + time;
 }
 
